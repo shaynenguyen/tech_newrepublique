94e5e584244972db9b74627bd3f3576f3d52d69f
ShayneNg <phamhai1986@yahoo.com>
2020-02-03 05:52:10 +1100

    second commit

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     // invoke the carousel
     $('#myCarousel').carousel({
-        interval: 6000
+        interval: false
     });
 
     // scroll slides on mouse scroll 
@@ -9,7 +9,6 @@ $(document).ready(function() {
 
         if (e.originalEvent.wheelDelta > 0 || e.originalEvent.detail < 0) {
             $(this).carousel('prev');
-
 
         } else {
             $(this).carousel('next');
@@ -36,48 +35,4 @@ $(document).ready(function() {
         });
     });
 
-});
-//animated  carousel start
-$(document).ready(function() {
-
-    //to add  start animation on load for first slide 
-    $(function() {
-        $.fn.extend({
-            animateCss: function(animationName) {
-                var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-                this.addClass('animated ' + animationName).one(animationEnd, function() {
-                    $(this).removeClass(animationName);
-                });
-            }
-        });
-        $('.item1.active img').animateCss('slideInDown');
-        $('.item1.active h2').animateCss('zoomIn');
-        $('.item1.active p').animateCss('fadeIn');
-
-    });
-
-    //to start animation on  mousescroll , click and swipe
-
-
-
-    $("#myCarousel").on('slide.bs.carousel', function() {
-        $.fn.extend({
-            animateCss: function(animationName) {
-                var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-                this.addClass('animated ' + animationName).one(animationEnd, function() {
-                    $(this).removeClass(animationName);
-                });
-            }
-        });
-
-        // add animation type  from animate.css on the element which you want to animate
-
-        $('.item1 img').animateCss('slideInDown');
-        $('.item1 h2').animateCss('zoomIn');
-        $('.item1 p').animateCss('fadeIn');
-
-        $('.item2 img').animateCss('zoomIn');
-        $('.item2 h2').animateCss('swing');
-        $('.item2 p').animateCss('fadeIn');
-    });
 });
